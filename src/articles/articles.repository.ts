4802f5e8ea4e4cd382b533529c4da4463774668c
Alexandru Bese <alexandrubese@Alexandrus-MacBook-Pro.local>
2020-10-24 23:23:53 +0300

    Refactored DocClient outside of Repo and injected as a dependency

--- src/articles/articles.repository.ts
+++ src/articles/articles.repository.ts
@@ -2,13 +2,12 @@ import { AWSError, DynamoDB } from 'aws-sdk';
 import { PromiseResult } from 'aws-sdk/lib/request';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import { Article, GetArticlesResult } from './articles.interfaces';
-import { DynamoService } from '../../shared/dynamo-service';
 
 export class ArticlesRepository {
   private readonly docClient: DocumentClient;
 
-  constructor() {
-    this.docClient = new DynamoService().getInstance();
+  constructor(docClient: DocumentClient) {
+    this.docClient = docClient;
   }
 
   public async getArticles(): Promise<GetArticlesResult> {
--- src/articles/articles.ts
+++ src/articles/articles.ts
@@ -2,8 +2,11 @@ import { ApiHandler } from '../../shared/api.interfaces';
 import { ArticlesController } from './articles.controller';
 import { ArticlesRepository } from './articles.repository';
 import { ArticlesService } from './articles.service';
+import { DynamoService } from '../../shared/dynamo-service';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-const repo: ArticlesRepository = new ArticlesRepository();
+const docClient: DocumentClient = new DynamoService().getInstance();
+const repo: ArticlesRepository = new ArticlesRepository(docClient);
 const service: ArticlesService = new ArticlesService(repo);
 const controller: ArticlesController = new ArticlesController(service);
 
